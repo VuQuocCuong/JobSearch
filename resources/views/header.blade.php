46663f3308a155993505e0597cf080c67c508767
cuongduong <duongthaicuong@gmail.com>
2016-04-12 22:27:41 +0700

    Init header source

---
+++ resources/views/header.blade.php
@@ -0,0 +1,10 @@
+<?php $user = Session::get('user'); ?>
+
+<div id="header">
+	<div id="header-left">
+		<a href="/"><img src="/img/logo.png" border="0" alt="Social Job Search" title="Social Job Search"></a>
+		<div class="clr"><br></div>
+	</div>
+	
+</div>
+<div class="clr"></div>
