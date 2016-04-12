0a138adfe60dc19d3c24c23d54894f0d9837a7ea
cuongduong <duongthaicuong@gmail.com>
2016-04-12 22:23:04 +0700

    Init js and img file

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<head>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<meta name="keywords" content="social job search" />
+	<meta name="description" content="Web application for social it job search free" />
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no">
+	<title>Social IT Job Search</title>
+	<!-- Style Sheet -->
+	<link rel="stylesheet" href="{{ asset('css/form.css') }}" />
+	<link rel="stylesheet" href="{{ asset('css/design.css') }}" />
+	<link rel="stylesheet" href="{{ asset('css/custom.css') }}" />
+	<link rel="stylesheet" href="{{ asset('css/jquery-ui.css') }}" />
+	<!-- Javascript -->
+	<script language="JavaScript" type="text/javascript" src="{{ asset('js/jquery.js') }}"></script>
+	<script language="JavaScript" type="text/javascript" src="{{ asset('js/jquery-ui.js') }}"></script>
+</head>
+<body>
+	<div class="main-wrapper sub">
+		<div id="loading"></div>
+		<div id="messageBox"></div>
+		<div id="header-bg">
+			<div id="header-bg-in"></div>
+		</div>
+		<div class="main-div">
+			@include('header')
+			@include('search')
+
+			<!-- Content -->
+			@yield('content')
+		</div>
+		<!-- Footer -->
+		@include('footer')
+	</div>
+</body>
+</html>
