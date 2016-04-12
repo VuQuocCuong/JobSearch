8ace58fbc89726448951a30ab97ff9ba2f349f09
cuongduong <duongthaicuong@gmail.com>
2016-04-12 22:27:50 +0700

    Init search source

---
+++ resources/views/search.blade.php
@@ -0,0 +1,8 @@
+<div id="quick-search">
+	<div id="quick-search-left">
+		<div class="label"><h1>Find a Job</h1></div>
+		<div class="right-arrow"></div>
+	</div>
+	
+</div>
+<div class="clr"></div>
